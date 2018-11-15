b19b459462b5db325d23a27138d0988ab551db8d
Juan Botter <juan.botter@gmail.com>
2018-11-15 06:37:48 -0300

    Mover unidades

--- TP2-Algo-Empires/src/aplicacion/entidades/Entidad.java
+++ TP2-Algo-Empires/src/aplicacion/entidades/Entidad.java
@@ -1,15 +1,26 @@
 package entidades;
 
+import mapa.Mapa;
+
 import java.awt.Point;
 
 public abstract class Entidad{
+
     private int vida;
 	private Point tamanio;
-	
-	public Entidad(int vida, Point tamanio) {
+	private Point posicion;
+    private Mapa mapa;
+
+    public Mapa getMapa(){
+        return this.mapa;
+    }
+
+
+    public Entidad(int vida, Point tamanio) {
 		this.vida = vida;
 		this.tamanio = tamanio;
 	}
+
 	public int vida() {
     	return this.vida;
     }
@@ -18,5 +29,17 @@ public abstract class Entidad{
 	}
 	public void setVida(int vida){
 	    this.vida = vida;
+    }
+
+	public void setPosicion(Point posicion){
+	    this.posicion = posicion;
+    }
+
+    public Point verPosicion(){
+	    return this.posicion;
+    }
+
+    public void setMapa(Mapa mapa){
+	    this.mapa = mapa;
     }
 }
--- TP2-Algo-Empires/src/aplicacion/entidades/unidades/Unidad.java
+++ TP2-Algo-Empires/src/aplicacion/entidades/unidades/Unidad.java
@@ -2,20 +2,35 @@ package entidades.unidades;
 
 import entidades.Entidad;
 import entidades.edificios.Edificio;
+import excep.DireccionInvalidaException;
 import excepciones.NoPuedeRepararException;
+import mapa.Mapa;
 
 import java.awt.Point;
 
 public abstract class Unidad extends Entidad {
+    private Mapa mapa;
+    private Point coordenadas;
 
 	public Unidad(int vida) {
 		super(vida, new Point(1,1	));
 	}
-	void desplazarse(Point direccion) {
-	}//se podria crear una clase direccion
-	public abstract Unidad crear_unidad();
+
+	public void desplazarse(Point direccion) {
+	    if(!direccionUnitariaEsValida(direccion)){
+	        throw new DireccionInvalidaException();
+        }
+        super.getMapa().mover(this, super.verPosicion(), direccion);
+
+
+	}
 
     public void reparar(Edificio edificio) {
         throw new NoPuedeRepararException();
+    }
+
+
+    private boolean direccionUnitariaEsValida(Point direccion){
+	    return (-1 <= direccion.getX() && direccion.getX() <= 1 && -1 <= direccion.getY() && direccion.getY() <= 1);
     }
 }
