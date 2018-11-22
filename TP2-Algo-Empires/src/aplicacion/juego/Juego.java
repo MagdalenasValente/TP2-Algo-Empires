832a3d9ac5aaec0d105495c1d3aa0b2ee150a7af
Juan Botter <juan.botter@gmail.com>
2018-11-22 17:47:47 -0300

    cambios en el paquete juego

--- TP2-Algo-Empires/src/aplicacion/juego/Juego.java
+++ TP2-Algo-Empires/src/aplicacion/juego/Juego.java
@@ -1,4 +1,32 @@
 package juego;
 
 public class Juego {
+    private Jugador jugadorDeTurno;
+    private Jugador jugadorOponente;
+
+    public Juego(String nombre1, String nombre2){
+        this.jugadorDeTurno = new Jugador(nombre1);
+        this.jugadorOponente = new Jugador(nombre2);
+    }
+
+    public void terminarTurno(){
+        this.jugadorDeTurno.terminarTurno();
+        if(this.acabo()){
+            //se acabo el juego
+        }
+        this.cambiarJugadores();
+
+    }
+
+    private void cambiarJugadores(){
+        Jugador aux;
+        aux = this.jugadorDeTurno;
+        this.jugadorDeTurno = this.jugadorOponente;
+        this.jugadorOponente = aux;
+    }
+
+    public boolean acabo(){
+        return this.jugadorOponente.haPerdido();
+    }
+
 }
